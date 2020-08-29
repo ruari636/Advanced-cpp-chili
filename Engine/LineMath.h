98a7445e8eb9167b45d2073496ff3b5af00ca5af
Ruari theakston <ruaritheakston@outlook.com>
2020-08-29 12:13:07 +0100

    Made linemath more efficient with right-angle rotation formulas.

--- Engine/LineMath.h
+++ Engine/LineMath.h
@@ -32,12 +32,32 @@ T GetDistSq(const _Vec2<T>& vert1, const _Vec2<T>& vert2, const _Vec2<T>& point)
 }
 
 template <typename T>
+_Vec2<T> FindIntersection(_Vec2<T> l0p0, _Vec2<T> l0p1, _Vec2<T> l1p0, _Vec2<T> l1p1)
+{
+	if (l0p1.x == l0p0.x)
+	{
+		T l1M = (l1p1.y - l1p0.y) / (l1p1.x - l1p0.x);
+		T l1C = l1p0.y - (l1M * l1p0.x);
+		return { l0p1.x, (l0p1.x * l1M) + l1C };
+	}
+	if (l1p1.x == l1p0.x)
+	{
+		T l0M = (l0p1.y - l0p0.y) / (l0p1.x - l0p0.x);
+		T l0C = l0p0.y - (l0M * l0p0.x);
+		return { l1p1.x, (l1p1.x * l0M) + l0C };
+	}
+	T l0M = (l0p1.y - l0p0.y) / (l0p1.x - l0p0.x);
+	T l1M = (l1p1.y - l1p0.y) / (l1p1.x - l1p0.x);
+	T l0C = l0p0.y - (l0M * l0p0.x);
+	T l1C = l1p0.y - (l1M * l1p0.x);
+	T x = (l1C - l0C) / (l0M - l1M);
+	T y = (l0M * x) + l0C;
+	return { x,y };
+}
+
+template <typename T>
 _Vec2<T> GetDistVec(const _Vec2<T>& vert1, const _Vec2<T>& vert2, const _Vec2<T>& point)
 {
-	T a = (vert1.y - vert2.y) / (vert1.x - vert2.x);
-	T c = vert1.y - (a * vert1.x);
-	T x = point.x;
-	T y = point.y;
 	_Vec2<T> v1p = point - vert1;
 	_Vec2<T> v2p = point - vert2;
 	_Vec2<T> v1v2 = vert2 - vert1;
@@ -51,38 +71,9 @@ _Vec2<T> GetDistVec(const _Vec2<T>& vert1, const _Vec2<T>& vert2, const _Vec2<T>
 	}
 	else
 	{
-		T lineLenSq = (square(a * x - y + c)) / (square(a) + (T)1);
-		T x;
-		T y;
-		if (a != 0.0f)
-		{
-			T len = sqrt(lineLenSq);
-			T newGrad = -(T)1 / a; //std::abs(-(T)1 / a);
-			T newC = point.y - (newGrad * point.x);
-			_Vec2<T> dir = _Vec2<T>((T)1, newGrad);
-			dir.SetLen(len);
-			if (point.x * a + c < point.y && dir.y < 0.0f)
-			{
-				dir.y = -dir.y;
-				dir.x = -dir.x;
-			}
-			else if (point.x * a + c > point.y && dir.y > 0.0f)
-			{
-				dir.y = -dir.y;
-				dir.x = -dir.x;
-			}
-			return dir;
-		}
-		else
-		{
-			y = sqrt(lineLenSq);
-			if (point.x * a + c > point.y)
-			{
-				y = -y;
-			}
-			x = (T)0;
-			return _Vec2<T>(x, y);
-		}
+		_Vec2<T> dir = _Vec2<T>(v1v2.y, -v1v2.x);
+		_Vec2<T> intersection = FindIntersection(vert1, vert2, point, point + dir);
+		return point - intersection;
 	}
 }
 
